7a496bd86598af88831566b8fe161d47742776f0
younghyun.j <zerohyun.j@gmail.com>
2025-01-13 11:14:58 +0900

    feat : project init

---
+++ src/main/java/com/cicdpractice/CicdPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.cicdpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdPracticeApplication.class, args);
+    }
+
+}
