8702470654565a1b8d7e6cacb83ad7476c537724
younghyun.j <zerohyun.j@gmail.com>
2025-01-19 20:36:02 +0900

    test : 두번째 테스트 클래스 생성

---
+++ src/main/java/com/cicdpractice/CiTest2.java
@@ -0,0 +1,12 @@
+package com.cicdpractice;
+
+public class CiTest2 {
+    public String test(String input) {
+        return switch (input) {
+            case "hello" -> "world";
+            case "jpa" -> "hibernate";
+            case "spring" -> "boot";
+            default -> "default";
+        };
+    }
+}
