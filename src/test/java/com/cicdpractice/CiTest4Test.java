1a1b9ad25097e624e7ab331c7c500d03cb2b0cb7
younghyun.j <zerohyun.j@gmail.com>
2025-01-19 21:58:17 +0900

    test : Test4 테스트 2개 추가 실행

---
+++ src/test/java/com/cicdpractice/CiTest4Test.java
@@ -0,0 +1,36 @@
+package com.cicdpractice;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CiTest4Test {
+
+    @DisplayName("hello 매개변수를 전달하면 world 값을 반환한다")
+    @Test
+    void hello_world_test() throws Exception {
+        //given
+        CiTest4 ciTest = new CiTest4();
+
+        //when
+        String result = ciTest.test("hello");
+
+        //then
+        Assertions.assertEquals("world", result);
+    }
+
+    @DisplayName("jpa 매개변수를 전달하면 hibernate 값을 반환한다")
+    @Test
+    void jpa_hibernate_test() throws Exception {
+        //given
+        CiTest4 ciTest = new CiTest4();
+
+        //when
+        String result = ciTest.test("jpa");
+
+        //then
+        Assertions.assertEquals("hibernate", result);
+    }
+}
